dbeb6c084ff25c4ce9317fc42fc407847c12ba93
Enrique Barbeito García <ebarbeito@gmail.com>
2020-04-05 13:18:29 +0200

    Add some scaffold to run tests and web

---
+++ tests/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ebarbeito\Php\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
